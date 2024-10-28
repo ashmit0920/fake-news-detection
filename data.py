1d9060931e9e90cef89bdfcc5e47e847d6263956
unknown <ashmitthawait2@gmail.com>
2024-10-28 22:14:58 +0530

    gossipcop dataset preprocessing

--- data.py
+++ data.py
@@ -1,5 +1,20 @@
 import pandas as pd
 import numpy as np
 
-data = pd.read_csv("./fakeNewsNet_dataset/gossipcop_real.csv")
-print(data.head())
+real = pd.read_csv("./fakeNewsNet_dataset/gossipcop_real.csv")
+fake = pd.read_csv("./fakeNewsNet_dataset/gossipcop_fake.csv")
+
+fake = fake[['title']].copy()
+fake['label'] = 1  # 1 for fake news
+
+real = real[['title']].copy()
+real['label'] = 0  # 0 for real news
+
+# Combine both datasets
+combined_df = pd.concat([fake, real], ignore_index=True)
+
+# Shuffle the combined dataset to mix fake and real news
+combined_df = combined_df.sample(frac=1).reset_index(drop=True)
+print(combined_df['label'].value_counts())
+
+combined_df.to_csv("final_dataset.csv")
